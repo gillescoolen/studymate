36d849793f4970eff84291521f77d3ab6bdcd668
Dwiky <dwikboss99@gmail.com>
2020-03-16 16:11:16 +0100

    Add teacher relation migrations

---
+++ database/migrations/2020_03_16_150654_add_teacher_relation_module.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTeacherRelationModule extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('module', function (Blueprint $table) {
+            $table->integer('teacher_id')->unsigned()->nullable();
+            $table->foreign('teacher_id')->references('id')->on('teacher');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
