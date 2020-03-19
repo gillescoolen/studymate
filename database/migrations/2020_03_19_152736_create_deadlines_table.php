ab8fc5f1910656ba640b14c39a0ee937fe4716cb
Gilles Coolen <gilleswhc@gmail.com>
2020-03-19 20:17:12 +0100

    Add deadlines with tags to database

---
+++ database/migrations/2020_03_19_152736_create_deadlines_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeadlinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deadlines', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('date');
+            $table->integer('final_id')->unsigned();
+            $table->foreign('final_id')->references('id')->on('final');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deadlines');
+    }
+}
---
+++ database/migrations/2020_03_19_162254_create_deadline_tags_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeadlineTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deadline_tags', function (Blueprint $table) {
+            $table->integer('deadline')->unsigned();
+            $table->string('tag');
+            $table->foreign('deadline')->references('id')->on('deadlines')
+                ->onDelete('cascade');
+            $table->foreign('tag')->references('name')->on('tags')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deadline_tags');
+    }
+}
