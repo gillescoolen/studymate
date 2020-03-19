d4ce5c7516bbe95f69ca680c53053ba96f51b737
Dwiky <dwikboss99@gmail.com>
2020-03-19 18:42:38 +0100

    Added role gate

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Studymate</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -46,7 +46,6 @@
                     </div>
                 </div>
             </nav>
-
             <div class="container">
                 @yield('content')
             </div>
