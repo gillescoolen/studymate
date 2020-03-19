80377e1487cf7af6a7e9b93e3990597869d6a7e8
Dwiky <dwikboss99@gmail.com>
2020-03-19 15:45:11 +0100

    Added auth to laravel

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,6 @@ Route::post('/add-teacher', 'TeacherController@store');
 
 // Exam
 Route::post('/add-exam', 'ExamController@store');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
