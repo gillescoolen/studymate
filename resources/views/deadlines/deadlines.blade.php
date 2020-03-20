8f6fcf1a0be3a4497bafe9877ef31a5f8086c7e8
Gilles Coolen <gilleswhc@gmail.com>
2020-03-20 20:31:24 +0100

    Add more columns to deadline overview

--- resources/views/deadlines/deadlines.blade.php
+++ resources/views/deadlines/deadlines.blade.php
@@ -6,14 +6,26 @@
                 <th scope="col">Naam</th>
                 <th scope="col">Vak</th>
                 <th scope="col">Type</th>
-                <th scope="col">Datum</th>
+                <th scope="col">Docent</th>
+                <th scope="col">Categorieën</th>
+                <th scope="col">Gedaan</th>
+                <th scope="col"></th>
             </tr>
         </thead>
         <tbody>
-            @foreach($deadlines as $exam)
+            @foreach($deadlines as $deadline)
+                <form id="deadline-form" action="{{ action('DeadlineController@store') }}" method="POST">
+                    @CSRF
                     <tr>
-                        <td style="width: 25%">{{$exam->date}}</td>
+                        <td style="width: 25%">{{$deadline->date}}</td>
+                        <td style="width: 25%">{{$deadline->exam->name}}</td>
+                        <td style="width: 25%">{{$deadline->exam->type->type}}</td>
+                        <td style="width: 25%">{{$deadline->exam->module->teacher->firstname}}</td>
+                        <td style="width: 25%">{{$deadline->tag}}</td>
+                        <td style="width: 25%"><input type="checkbox" name="done" value="{{$deadline->id}}"></td>
+                        <td style="width: 25%"><button type="submit" class="btn btn-sm btn-primary">Save</button></td>
                     </tr>
+                </form>
             @endforeach
         </tbody>
     </table>
