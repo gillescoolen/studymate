e6031d140725e4893a57a82addd3d14fa1b16f44
Gilles Coolen <gilleswhc@gmail.com>
2020-03-23 13:02:40 +0100

    Remove empty methods and add request validation

--- app/Http/Controllers/ModuleController.php
+++ app/Http/Controllers/ModuleController.php
@@ -10,55 +10,19 @@ use Illuminate\Http\Request;
 
 class ModuleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(ModuleRequest $request)
     {
+        $request->validate([
+            'name' => 'required|string|max:255|filled',
+            'ec' => 'required|integer|filled',
+            'period_id' => 'required|integer|filled',
+            'teacher_id' => 'required|integer|filled'
+        ]);
+
         Module::create($request->all());
         return redirect()->route('admin')->with('message', 'New module created successfully!');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         $module = Module::find($id);
@@ -68,25 +32,19 @@ class ModuleController extends Controller
         return view('admin.edit.edit-module', ['module' => $module, 'periods' => $periods, 'teachers' => $teachers]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'name' => 'required|string|max:255|filled',
+            'ec' => 'required|integer|filled',
+            'period_id' => 'required|integer|filled',
+            'teacher_id' => 'required|integer|filled'
+        ]);
+
         Module::find($id)->update($request->all());
-        return redirect()->route('admin')->with('message', 'New module updated successfully!');
+        return redirect()->route('admin')->with('message', 'De nieuwe module is aangemaakt!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         Module::find($id)->delete();
