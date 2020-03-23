dd1bac9a636c1cdb3240925a9a39894a276434d2
Gilles Coolen <gilleswhc@gmail.com>
2020-03-23 13:33:26 +0100

    Add teacher request validation

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -8,84 +8,37 @@ use Illuminate\Http\Request;
 
 class TeacherController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(TeacherRequest $request)
     {
+        $request->validate([
+            'firstname' => 'required|string|max:255|min:1|filled',
+            'lastname' => 'required|string|max:255|min:1|filled',
+        ]);
+
         Teacher::create($request->all());
-        
-        return redirect()->route('admin')->with('message', 'New teacher created successfully!');
+        return redirect()->route('admin')->with('message', 'Nieuwe docent succesvol toegevoegd!');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         $teacher = Teacher::find($id);
         return view('admin.edit.edit-teacher', ['teacher' => $teacher]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'firstname' => 'required|string|max:255|min:1|filled',
+            'lastname' => 'required|string|max:255|min:1|filled',
+        ]);
+
         Teacher::find($id)->update($request->all());
-        return redirect()->route('admin')->with('message', 'Teacher updated successfully!');
+        return redirect()->route('admin')->with('message', 'Docent succesvol geupdate!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         Teacher::find($id)->delete();
-        return redirect()->route('admin')->with('message', 'Teacher deleted successfully!');
+        return redirect()->route('admin')->with('message', 'Docent succesvol verwijderd!');
     }
 }
