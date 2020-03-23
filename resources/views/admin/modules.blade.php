f2308adbe620aa7a8a1e7af61709ced278fab087
Gilles Coolen <gilleswhc@gmail.com>
2020-03-23 14:06:25 +0100

    Decrease edit and delete button sizes

--- resources/views/admin/modules.blade.php
+++ resources/views/admin/modules.blade.php
@@ -23,8 +23,8 @@
                         <form action="{{ route('module.destroy', $module->id) }}" method="POST">
                             @csrf
                             @method('DELETE')
-                            <a class="btn btn-warning" href="{{ route('module.edit', $module->id) }}">Edit</a>
-                            <button id="submit" type="submit" class="btn btn-danger">X</button>
+                            <a class="btn btn-sm btn-warning" href="{{ route('module.edit', $module->id) }}">Edit</a>
+                            <button id="submit" type="submit" class="btn btn-sm btn-danger">X</button>
                         </form>
                     </td>
                 </tr>
