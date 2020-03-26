239e6b5cfaa614656d89899f739a2b1442d1dff1
Gilles Coolen <gilleswhc@gmail.com>
2020-03-26 20:06:01 +0100

    Fix broken deadline saving

--- app/Http/Requests/DeadlineRequest.php
+++ app/Http/Requests/DeadlineRequest.php
@@ -6,28 +6,28 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class DeadlineRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return $this->user()->role === 'manager';
-    }
+  /**
+   * Determine if the user is authorized to make this request.
+   *
+   * @return bool
+   */
+  public function authorize()
+  {
+    return $this->user()->role === 'manager';
+  }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            'date' => 'required|date|filled',
-            'tags' => 'array|filled',
-            'tags.name' => 'string|filled|min:1|max:255',
-            'exam_id' => 'integer|filled|gt:0'
-        ];
-    }
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array
+   */
+  public function rules()
+  {
+    return [
+      'date' => 'date|filled',
+      'tags' => 'array|filled',
+      'tags.name' => 'string|filled|min:1|max:255',
+      'exam_id' => 'integer|filled|gt:0'
+    ];
+  }
 }
